87e151d46309abc8a17f2f4eadb831f29bf9b25d
Andre <aloliveira98@outlook.com>
2025-03-29 17:24:55 +0000

    final version

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-// Click + Form
-document.addEventListener("DOMContentLoaded", function () {
+//wait the DOM load at full to start JS
+document.addEventListener("DOMContentLoaded", () => {
 
-    // Event 1: Click
-    const btn = document.getElementById("clickHere");
+     const fireBunny = document.getElementById("fireBunny");
 
-    if (btn) {
-      btn.addEventListener("click", function () {
-        alert("Bait, You have been hacked! \nJoking!! Haha ");
-      });
-    }
-  
-    // Event 2: Form Submission
+    if (fireBunny) {
+    fireBunny.addEventListener("dblclick", () => {
+      
+      document.body.style.backgroundColor = "orange";
+      alert(" now the fire Bunny is furious, be ready for a fight!");
+     });
+   }
+
+
+     // Event 1: Click
+     // call bottom with ID "clickHere"
+    
+     const button= document.getElementById("clickHere");
+    
+     //if bottom exist...
+
+     if (button) {
+       button.addEventListener("click", function () {
+         alert("Bait, You have been hacked! \nJoking!! Haha ");
+       });
+     }
+
+      // Event 2: Form Submission
+      // get a form and the message
     const form = document.getElementById("signupForm");
     const message = document.getElementById("formMessage");
   
     if (form) {
+      //if form exist, will run event "submit"
       form.addEventListener("submit", function (e) {
-        e.preventDefault();
+        e.preventDefault(); //check for no error!!
         const name = form.name;
         const email = form.email;
   
@@ -28,16 +45,20 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     }
+      //Event 3: MouseOver
+      //id focus title changing color when mouse get over there
+      const title = document.getElementById('title');
 
-    const gameTitle = document.getElementById('gameTitle');
+      title.addEventListener('mouseover', () => {
+        title.style.color = 'darkblue';
+        title.style.backgroundSize = '2rem';
+      });
+      
+      title.addEventListener('mouseout', () => {
+        title.style.color = '';
+      });
+      
 
-    gameTitle.addEventListener('mouseover', () => {
-      gameTitle.style.color = 'darkblue';
-      gameTitle.style.backgroundSize = '2rem';
-    });
-    gameTitle.addEventListener('mouseout', () => {
-      gameTitle.style.color= '';
-    });
+ 
 
-  });
-  
+});
